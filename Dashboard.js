1dcc8258df6c3d546fd19e01d06916cf59f3d335
Priya Ranjan <priyaranjan.653@gmail.com>
2020-05-18 13:50:17 +0530

    Update Dashboard.js

--- Dashboard.js
+++ Dashboard.js
@@ -1,22 +1,9 @@
-import React from 'react'
-//import Highcharts from 'highcharts'
-//import HighchartsReact from 'highcharts-react-official'
- 
-// const options = {
-//   title: {
-//     text: 'My chart'
-//   },
-//   series: [{
-//     data: [1, 2, 3]
-//   }]
-// }
- 
-const Dashboard = () => <div>
-//   <HighchartsReact
-//     highcharts={Highcharts}
-//     options={options}
-//   />
-      <h2>External dashboard component</h2>
-</div>
- 
-export default Dashboard
+var React = require('react');
+
+var Hello = React.createClass({ displayName: "Hello",
+  render: function () {
+    return React.createElement("h1", null, "Hello, remote react component!");
+  } });
+
+
+exports = Hello;
